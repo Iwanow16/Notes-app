f64e7608cd50b9eae652b5f6262ebc70813ecbd1
Iwanow16 <107834739+Iwanow16@users.noreply.github.com>
2024-10-16 15:21:30 +0300

    Lab 2

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,91 @@
 module.exports = function (app, db) {
-  app.post("/notes", (req, res) => {
+  // Добавление заметки
+  app.post("/notes/add", async (req, res) => {
+    const note = {
+      text: req.body.body,
+      title: req.body.title,
+    };
+
+    try {
+      const result = await db.collection("notes").insertOne(note);
+      console.log("NOTE", note);
+      res.send({
+        text: note.text,
+        title: note.title,
+        _id: result.insertedId,
+      });
+    } catch (err) {
+      console.log("ERROR", err);
+      res.send({
+        error: "An error has occurred",
+      });
+    }
+  });
+
+  // Получение всех заметок
+  app.get("/notes/all", async (req, res) => {
+    try {
+      const result = await db.collection("notes").find({}).toArray();
+      console.log("result:", result);
+      res.send(result);
+    } catch (err) {
+      console.log("An error has occurred", err);
+      res.send({
+        error: "An error has occurred",
+      });
+    }
+  });
+
+  const { ObjectId } = require("mongodb");
+
+  // Удаление заметки по id
+  app.delete("/notes/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+      const result = await db
+        .collection("notes")
+        .deleteOne({ _id: ObjectId.createFromHexString(id) });
+
+      console.log("Note deleted successfully");
+      res.status(200).json({
+        message: "Note deleted successfully",
+      });
+    } catch (err) {
+      console.error("Error deleting note:", err);
+      res.status(500).json({
+        error: "An error occurred while deleting the note",
+        details: err.message,
+      });
+    }
+  });
+
+  // Редактирование заметки по id
+  app.put("/notes/:id", async (req, res) => {
+    const id = req.params.id;
+    const updatedNote = req.body;
     console.log(req.body);
-    res.send("Hello");
+    try {
+      const result = await db
+        .collection("notes")
+        .updateOne(
+          { _id: ObjectId.createFromHexString(id) },
+          { $set: updatedNote }
+        );
+
+      console.log("Note updated successfully");
+      console.log(updatedNote);
+
+      res.status(200).json({
+        message: "Note updated successfully",
+        note: { id, ...updatedNote },
+      });
+    } catch (err) {
+      console.error("Error updating note:", err);
+      res.status(500).json({
+        error: "An error occurred while updating the note",
+        details: err.message,
+      });
+    }
   });
 };
--- server.js
+++ server.js
@@ -3,8 +3,36 @@ const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 const app = express();
 const port = 8000;
-app.use(bodyParser.urlencoded({ extended: true }));
-require("./app/routes")(app, {});
-app.listen(port, () => {
-  console.log("We are live on " + port);
+
+const uri =
+  "mongodb+srv://1:2@cluster0.gnuxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbName = "Cluster0";
+
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+(async function () {
+  let client;
+
+  try {
+    client = await MongoClient.connect(uri);
+    console.log("Connected correctly to server");
+
+    const db = client.db(dbName);
+    require("./app/routes")(app, db);
+
+    app.listen(port, () => {
+      console.log("We are live on " + port);
+    });
+  } catch (err) {
+    console.log(err.stack);
+  }
+})();
